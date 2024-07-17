892759cb9493d75d3d8a8ffa3dd5d4f0e4b43ed7
Claudio <claudiogsssantos@gmail.com>
2024-07-16 23:00:22 -0300

    feat: criando pasta controllers, e arquivo products.js, para criar os métodos HTTP

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require("express");
+const connection = require("./db/connection");
+const table= require('./db/table');
+
 
 
 const app = express();
 const port = 2000;
 
-app.listen(port, () => {
-  console.log("Servidor rodando")
-});
 
-app.get('/', (req, res) => {
-  res.status(200).json({"message": "Funcionou"})
-});
+
+connection.connect(erro => {
+  if (erro) {
+    console.log(erro.message);
+  }else{
+    table.init(connection);
+
+    app.listen(port, () => {
+      console.log(`Rodando ${port}`)
+    })
+    
+    app.get('/', (req, res) => {
+      res.status(200).json({"message":"Server is running"})
+    })
+  }
+})
