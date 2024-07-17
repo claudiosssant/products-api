21d10ea3db2a584ce58e042282a050c0a51a677a
Claudio <claudiogsssantos@gmail.com>
2024-07-17 17:33:02 -0300

    feat: ajustes nas mensagens de erro

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,12 @@ const connection = require("../db/connection");
 
 class Produtos {
   cadastrarProduto(req, res) {
-    const produtoCadastrado = { ...req };
+    const dataHoraAtual = new Date()
+      .toISOString()
+      .slice(0, 19)
+      .replace("T", " ");
+
+    const produtoCadastrado = { ...req, data: dataHoraAtual };
 
     const sql = "INSERT INTO Produtos SET ?";
 
@@ -41,11 +46,16 @@ class Produtos {
 
   deletarProduto(id, res) {
     const sql = `DELETE FROM Produtos WHERE id = ${id}`;
-    connection.query(sql, (erro) => {
-      if (erro) {
-        res.status(400).json(erro);
+
+    connection.query(sql, (error, result) => {
+      if (error) {
+        res.status(500).json({ message: `Falha ao deletar produto!` });
+      } else if (result.affectedRows == 0) {
+        res
+          .status(404)
+          .json({ message: `Produto com o ID ${id}, não existe!` });
       } else {
-        res.status(200).json({ message: "Produto apagado!" });
+        res.status(200).json({ message: `Produto ${id} foi apagado!` });
       }
     });
   }
--- db/table.js
+++ db/table.js
@@ -6,12 +6,10 @@ class tabela {
 
   listaProdutos() {
     const sql =
-      "CREATE TABLE IF NOT EXISTS Produtos (id int NOT NULL AUTO_INCREMENT, name varchar(50), price decimal(6, 2), description varchar(400) NOT NULL, data datetime NOT NULL, PRIMARY KEY(ID))";
+      "CREATE TABLE IF NOT EXISTS Produtos (id int NOT NULL AUTO_INCREMENT, name varchar(50) NOT NULL, price decimal(6, 2) NOT NULL, description varchar(400) NOT NULL, data DATETIME NOT NULL, PRIMARY KEY(ID))";
     this.connection.query(sql, (erro, retorno) => {
       if (erro) {
         console.log(erro);
-      } else {
-        console.log(retorno);
       }
     });
   }
